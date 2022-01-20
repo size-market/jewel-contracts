dcf4df186db328ddf8495cf77d3243f2d09705f7
calebcheng00 <50129617+calebcheng00@users.noreply.github.com>
2022-01-20 10:57:04 +0800

    feat: volume

--- src/JewelLens.sol
+++ src/JewelLens.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.11;
 
-import {IJewelToken, ILockedJewelOffer, IOfferFactory} from "./interfaces/Interfaces.sol";
+import {IERC20, IJewelToken, ILockedJewelOffer, IOfferFactory, IOwnable} from "./interfaces/Interfaces.sol";
 
 contract JewelLens {
     IJewelToken JEWEL = IJewelToken(0x72Cb10C6bfA5624dD07Ef608027E366bd690048F);
+
+    function getVolume(IOfferFactory factory) public view returns (uint sum){
+        uint volume;
+        address[5] memory stables = [0x224e64ec1BDce3870a6a6c777eDd450454068FEC, 0x3C2B8Be99c50593081EAA2A724F0B8285F5aba8f,
+                                    0x985458E523dB3d53125813eD68c274899e9DfAb4, 0xEf977d2f931C1978Db5F6747666fa1eACB0d0339,
+                                    0xE176EBE47d621b984a73036B9DA5d834411ef734];
+        for(uint i; i < stables.length; i++) {
+            volume += IERC20(stables[i]).balanceOf(IOwnable(address(factory)).owner()) * (10**(18 - IERC20(stables[i]).decimals()));
+        }
+        sum = volume * 40;
+    }
 
     function getOfferInfo(ILockedJewelOffer offer)
         public
